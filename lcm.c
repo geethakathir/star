22d714438a89a5511483dc882e3c89a46985a7c8
geethakathir <30830151+geethakathir@users.noreply.github.com>
2017-09-12 15:15:59 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,min;
+scanf("%d","%d",a,b);
+min=(a<b)?b:c;
+while(1)
+{
+if((a%min==0)&&(b%min==0))
+{
+printf("LCM of given no is %d",min);
+break;
+}
+min++;
+}
+}
