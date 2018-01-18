fd5a20d99664b16cb20d749ce18435bc53bf02b7
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-01-18 16:49:07 +0530

    Create fact1.c

---
+++ fact1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(i==2)
+{
+printf("the number is factorial");
+}
+else
+{
+printf("the number is not factoial");
+}
+}
