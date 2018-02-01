cfff85623392a7fd10ff1db08fc6d88e6e095767
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-01 08:08:23 +0000

    Create str6.c

---
+++ str6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int cmpstr(char s1[10],char s2[10];
+int main()
+{
+char a1[10]="north";
+char a2[10]="south";
+printf("%d",cmpstr(a1,a2));
+int i=str1;
+int j=str2;
+int i=str1;
+printf("%d",a str 1);
+printf("%d",b str 2);
+}
+
