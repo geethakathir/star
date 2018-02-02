287987ec1941f59b415ff850a9e11ed88156e293
geethakathir <30830151+geethakathir@users.noreply.github.com>
2018-02-02 14:08:57 +0530

    Create str68.c

---
+++ str68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,a,b,count=0;
+char a[10]="waft";
+char b[20]="program";
+for(i=0;i<=5;i++)
+{
+printf("\n enter the string");
+scanf("%s",&a);
+printf("\n the second string is");
+scanf("%s",&b);
+printf("\n the string value is");
+}
+}
